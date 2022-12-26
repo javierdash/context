3b392c3d8a6e965f8629085dc99d218baf7155b8
Javier Herzkovich <javierdash@gmail.com>
2022-12-26 12:39:45 -0300

    cambiando a version 17 de react y 5.0 de react-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import {BrowserRouter} from 'react-router-dom'
-import Home from "./pages/Home/Home"
-import Product from "./pages/Product/Product"
-import Products from "./pages/Products/Products"
+import Routes from "./routes"
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+      <Routes />
   );
 }
 
 export default App;
+
+// import {BrowserRouter as Router, Route} from 'react-router-dom'
+// import Home from "./pages/Home/Home"
+// import Product from "./pages/Product/Product"
+// import Products from "./pages/Products/Products"
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Route exact path="/" element={Home} />
+//       <Route path="/product" element={Product} />
+//       <Route path="/products" element={Products} />
+//     </Router>
+//   )
+// }
+
+// export default App
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,19 @@
 //podria llamarse publicRoutes.js
 //este es el entry point de todas las rutas de mi app
+// BrowserRouter => es un contexto que nos provee info. Es un estado global.
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Home from "../pages/Home/Home"
+import Product from "../pages/Product/Product"
+import Products from "../pages/Products/Products"
 
+const Routes = () => {
+  return (
+    <Router>
+      <Route exact path="/" render={Home} />
+      <Route path="/products" render={Product} />
+      <Route path="/products/1" render={Products} />
+    </Router>
+  )
+}
 
+export default Routes
